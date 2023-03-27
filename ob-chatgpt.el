b30021162122d8717f7fbbd5b07096d88d119374
yhiraki <coffexpr@gmail.com>
2023-03-27 23:31:57 +0900

    chat context

--- ob-chatgpt.el
+++ ob-chatgpt.el
@@ -1,21 +1,25 @@
+(defcustom ob-chatgpt-api-token "")
+
+(defvar org-babel-default-header-args:chatgpt
+  '((:role . "user")
+	(:wrap . "src markdown :role assistant")))
+
 (defun ob-chatgpt-build-command (body)
   (mapconcat
    #'shell-quote-argument
    `("curl" "https://api.openai.com/v1/chat/completions"
 	 "-s"
 	 "-H" "Content-Type: application/json"
-	 "-H" ,(format "Authorization: Bearer %s" org-ai-openai-api-token)
+	 "-H" ,(format "Authorization: Bearer %s" ob-chatgpt-api-token)
 	 "-d" ,(json-encode-alist
 			`(:model "gpt-3.5-turbo" :messages ,(ob-chatgpt-get-all-code-blocks)))
- ) " ")
-  )
+ ) " "))
 
 (defun ob-chatgpt-execute-command (cmd)
   "Exec CMD and extract response."
   (let* ((result (shell-command-to-string (ob-chatgpt-build-command body)))
 		 (response (json-read-from-string result)))
-	(cdr (assq 'content (car (aref (cdr (assq 'choices response)) 0))))
-	))
+	(cdr (assq 'content (car (aref (cdr (assq 'choices response)) 0))))))
 
 (defun org-babel-execute:chatgpt (body params)
   "Execute a block of Chat."
@@ -24,11 +28,17 @@
 (defun ob-chatgpt-get-all-code-blocks ()
   "Get all code blocks for chat."
   (interactive)
-  (let ((blocks '()))
+  (let* ((blocks '())
+		(current-info (org-babel-get-src-block-info))
+		(current-value (nth 1 current-info))
+		(stop nil))
 	(org-babel-map-src-blocks (buffer-file-name)
 	  (let* ((info (org-babel-get-src-block-info))
 			 (role (assq :role (nth 2 info)))
 			 (value (nth 1 info)))
-		(when role
-		  (push `(,role (:content . ,value)) blocks))))
-	blocks))
+		(when (and role (not stop))
+		  (push `(,role (:content . ,value)) blocks))
+		(when (string= value current-value)
+		  (setq stop t))
+		))
+	(reverse blocks)))
