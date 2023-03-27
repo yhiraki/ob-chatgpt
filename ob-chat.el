abd11cd1f473811bf307e0f4b0011cc3bb6ff423
yhiraki <coffexpr@gmail.com>
2023-03-27 11:32:44 +0900

    update

--- ob-chat.el
+++ ob-chat.el
@@ -2,16 +2,22 @@
   (mapconcat
    #'shell-quote-argument
    `("curl" "https://api.openai.com/v1/chat/completions"
+	 "-s"
 	 "-H" "Content-Type: application/json"
 	 "-H" ,(format "Authorization: Bearer %s" org-ai-openai-api-token)
-	 "-d" "'{ \"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\", \"content\": \"Server-Sent Eventについて教えて\"}] }'"
+	 "-d" ,(json-encode-alist
+			`(:model "gpt-3.5-turbo" :messages [(:role "user" :content ,body)])
+			)
  ) " ")
   )
-(ob-chat-ai-build-command "a")
+
+(defun ob-chat-ai-execute-command (cmd)
+  "Exec CMD and extract response."
+  (let* ((result (shell-command-to-string (ob-chat-ai-build-command body)))
+		 (response (json-read-from-string result)))
+	(cdr (assq 'content (car (aref (cdr (assq 'choices response)) 0))))
+	))
 
 (defun org-babel-execute:chat-ai (body params)
   "Execute a block of Python code with org-babel."
-  (shell-command-to-string (ob-chat-ai-build-command body)))
-
-(require 'json)
-(json-encode-alist '(:model "gpt-3.5-turbo"))
+  (ob-chat-ai-execute-command (ob-chat-ai-build-command body)))
