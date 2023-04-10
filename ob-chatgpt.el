60fade38baceeb390189f526d75cb996ccb8b975
yhiraki <coffexpr@gmail.com>
2023-04-10 14:34:24 +0900

    WIP async execute

--- ob-chatgpt.el
+++ ob-chatgpt.el
@@ -20,34 +20,50 @@
 	(:session . "default")
 	))
 
+(defun org-babel-chatgpt-build-command (body)
+  "Build a command using BODY for fetch OpenAI API."
+  (mapcar
+   #'shell-quote-argument
+   `("curl" "https://api.openai.com/v1/chat/completions"
+	 "-s"
+	 "-H" "Content-Type: application/json"
+	 "-H" ,(format "Authorization: Bearer %s" org-babel-chatgpt-api-token)
+	 "-d" ,(json-encode-alist
+			`(:model ,org-babel-chatgpt-model :messages ,(org-babel-chatgpt-get-all-code-blocks))))
+   ))
+
 (defun org-babel-chatgpt-execute-command (cmd)
   "Exec CMD and extract response."
   (unless org-babel-chatgpt-api-token
 	(error "API TOKEN is not set.  Please set a value for `org-babel-chatgpt-api-token`"))
-  (let* ((url-request-method "POST")
-		 (url-request-extra-headers
-		  `(("Content-Type" . "application/json")
-			("Authorization" . ,(format "Bearer %s" org-babel-chatgpt-api-token))))
-		 (url-request-data (json-encode-alist
-							`(:model ,org-babel-chatgpt-model :messages ,(org-babel-chatgpt-get-all-code-blocks)))))
-	(message url-request-data)
-	(with-current-buffer (url-retrieve-synchronously "https://api.openai.com/v1/chat/completions")
-	  (goto-char (point-min))
-	  (search-forward-regexp "\n\n")
-	  (let* ((response-json (buffer-substring-no-properties (point) (point-max)))
-			 (data (json-read-from-string response-json)))
-		(message response-json)
-		(cdr (assq 'content (car (aref (cdr (assq 'choices data)) 0))))))))
+  (let* ((result (shell-command-to-string cmd))
+		 (response (json-read-from-string result)))
+	(message "%s" response)
+	(cdr (assq 'content (car (aref (cdr (assq 'choices response)) 0))))))
 
 (defun org-babel-execute:chatgpt (body params)
   "Execute a block of ChatGPT."
-  (let ((result (org-babel-chatgpt-execute-command (org-babel-chatgpt-build-command body))))
+  (let ((result (org-babel-chatgpt-execute-command (s-join " " (org-babel-chatgpt-build-command body)))))
 	(concat
 	 "#+BEGIN_SRC markdown "
 	 (format ":session %s :role assistant\n" (cdr (assq :session params)))
 	 result "\n"
 	 "#+END_SRC\n"
 	 )))
+
+(require 'async)
+
+(defun org-babel-execute:chatgpt-async (body params)
+  ""
+  (let* ((cmd (org-babel-chatgpt-build-command body))
+		 (args (append
+				`("chatgpt"
+				  ,(car cmd)
+				  (lambda (x)
+					(message "ret:%s" (buffer-string))))
+				(cdr cmd))))
+	(apply #'async-start-process args)))
+
 
 (defun org-babel-chatgpt-get-all-code-blocks ()
   "Get all code blocks for chat."
