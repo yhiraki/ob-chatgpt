4f9abcb59dc89a61ad8e260d4977dcb742fa8c75
yhiraki <coffexpr@gmail.com>
2023-09-01 21:36:09 +0900

    integrate with chatgpt.el

--- ob-chatgpt.el
+++ ob-chatgpt.el
@@ -16,13 +16,14 @@
 (require 'json)
 (require 'ob)
 (require 's)
+(require 'chatgpt)
 
 (defcustom org-babel-chatgpt-model "gpt-3.5-turbo"
   "Default model."
   :type 'string
   :group 'org-babel-chatgpt)
 
-(defcustom org-babel-chatgpt-api-token nil
+(defcustom org-babel-chatgpt-api-token chatgpt-api-token
   "OpenAPI token."
   :type 'string
   :group 'org-babel-chatgpt)
@@ -51,59 +52,37 @@
     (:to-org . nil)
     ))
 
-(defun org-babel-chatgpt-build-command (messages model)
-  "Build a command using BODY for fetch OpenAI API."
-  (let* ((info (org-babel-get-src-block-info)))
-    (mapcar
-     #'shell-quote-argument
-     `("curl" "https://api.openai.com/v1/chat/completions"
-       "-s"
-       "-H" "Content-Type: application/json"
-       "-H" ,(format "Authorization: Bearer %s" org-babel-chatgpt-api-token)
-       "-d" ,(json-encode-alist
-              `(:model ,model :messages ,messages)))
-     )))
-
 (defun org-babel-chatgpt-add-backticks-spaces (str)
   "Add spaces around backticks in STR."
   (replace-regexp-in-string " ?`\\([^`'\n]+\\)` ?" " `\\1` " str))
-
-(defun org-babel-chatgpt-execute-command (cmd)
-  "Exec CMD and extract response."
-  (unless org-babel-chatgpt-api-token
-    (error "API TOKEN is not set.  Please set a value for `org-babel-chatgpt-api-token`"))
-  (let* ((result (shell-command-to-string cmd))
-         (response (json-read-from-string result)))
-    (message "%s" cmd)
-    (message "%s" response)
-    (cdr (assq 'content (assq 'message (aref (cdr (assq 'choices response)) 0))))))
 
 (defun org-babel-execute:chatgpt (body params)
   "Execute a block of ChatGPT."
   (let* ((current-thread (cdr (assq :thread params)))
          (messages (org-babel-chatgpt-get-chat-thread current-thread body))
-         (model (or (cdr (assq :model params)) org-babel-chatgpt-model))
-         (cmd (s-join " " (org-babel-chatgpt-build-command messages model)))
-         (result (org-babel-chatgpt-execute-command cmd))
-         (to-org (cdr (assq :to-org params))))
-    (if to-org
-        (progn
-          (let* ((in-file (org-babel-temp-file "chatgpt-md-"))
-                 (out-file (org-babel-temp-file "chatgpt-org-"))
-                 (cmd (mapconcat
-                       #'identity
-                       `("pandoc"
-                         "-t" "org"
-                         "-f" "markdown"
-                         ,in-file)
-                       " ")))
-            (unless (executable-find "pandoc")
-              (error "Command \"pandoc\" not found"))
-            (with-temp-file in-file (insert (org-babel-chatgpt-add-backticks-spaces result)))
-            (message "%s" cmd)
-            (org-babel-eval cmd "")
-            ))
-      result)))
+         (model (or (cdr (assq :model params)) org-babel-chatgpt-model)))
+    "..."
+    ))
+
+    ;;; old
+    ;; (if to-org
+    ;;     (progn
+    ;;       (let* ((in-file (org-babel-temp-file "chatgpt-md-"))
+    ;;              (out-file (org-babel-temp-file "chatgpt-org-"))
+    ;;              (cmd (mapconcat
+    ;;                    #'identity
+    ;;                    `("pandoc"
+    ;;                      "-t" "org"
+    ;;                      "-f" "markdown"
+    ;;                      ,in-file)
+    ;;                    " ")))
+    ;;         (unless (executable-find "pandoc")
+    ;;           (error "Command \"pandoc\" not found"))
+    ;;         (with-temp-file in-file (insert (org-babel-chatgpt-add-backticks-spaces result)))
+    ;;         (message "%s" cmd)
+    ;;         (org-babel-eval cmd "")
+    ;;         ))
+    ;;   result)))
 
 (defun org-babel-chatgpt-read-src-block-result-value ()
   "Read result block."
